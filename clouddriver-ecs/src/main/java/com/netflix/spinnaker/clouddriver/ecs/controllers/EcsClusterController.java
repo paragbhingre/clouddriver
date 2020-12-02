22e80624cf06870bc9a85785b443ab4f968bcdd1
Parag Bhingre <pbhingre@gmail.com>
2020-12-02 15:42:06 -0800

    fix path variable issue

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/controllers/EcsClusterController.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/controllers/EcsClusterController.java
@@ -22,7 +22,6 @@ import com.netflix.spinnaker.clouddriver.ecs.provider.view.EcsClusterProvider;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,10 +39,9 @@ public class EcsClusterController {
     return ecsClusterProvider.getAllEcsClusters();
   }
 
-  @RequestMapping(value = {"/ecs/ecsDescribeClusters"})
+  @RequestMapping(value = {"/ecs/ecsDescribeClusters/{account}/{region}"})
   public Collection<Cluster> getAllEcsClustersDescription(
-      @RequestParam(value = "account", required = true) String account,
-      @RequestParam(value = "region") String region) {
+      @PathVariable String account, @PathVariable String region) {
     return ecsClusterProvider.getAllEcsClustersDescription(account, region);
   }
 }
